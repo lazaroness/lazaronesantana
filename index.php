59921e8b3d1a19eb4e2517602fbffb43eb6d11b7
lazarone <lazarone@objectdata.com.br>
2016-06-01 14:00:42 -0300

    ajustes

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
-<?php
-  echo "<div style='width: 100%; text-align: center;'>";
-    echo "<h1>Site em  construção.</h1>";
-    echo "E-mail: <a href='mailto:lazarone.info.web@gmail.com'>lazarone.info.web@gmail.com</a><br />";
-  echo "</div>";
-?>
+<!DOCTYPE html>
+<html lang='pt-br'>
+  <head>
+    <link rel='shortcut icon' href='img/favicon.ico' />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Lazarone Santana</title>
+  </head>
+  <body>
+    <div style='width: 100%; text-align: center;'>
+      <h1>Site em  construção.</h1>
+      E-mail: <a href='mailto:lazarone.info.web@gmail.com'>lazarone.info.web@gmail.com</a><br />
+    </div>
+  <body>
+  </body>
+</html>
