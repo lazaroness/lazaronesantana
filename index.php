153dc8d240ce2a6656e2a12262286e3fc5b2a268
lazarone <lazarone@objectdata.com.br>
2016-06-01 13:48:20 -0300

    arquivos extras

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
-  echo "Site em  construção.";
+  echo "<div style='width: 100%; text-align: center;'>";
+    echo "<h1>Site em  construção.</h1>";
+    echo "E-mail: <a href='mailto:lazarone.info.web@gmail.com'>lazarone.info.web@gmail.com</a><br />";
+  echo "</div>";
 ?>
