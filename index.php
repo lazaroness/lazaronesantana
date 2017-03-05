f95c15996bf2049f7c0fff7144f585bee1ff9202
Lazarone Santos Santana <lazarone@localhost.localdomain>
2017-03-05 12:56:23 -0300

    base

--- index.php
+++ index.php
@@ -1,17 +1,94 @@
 <!DOCTYPE html>
 <html lang='pt-br'>
   <head>
-    <link rel='shortcut icon' href='img/favicon.ico' />
+    <title>Lazarone S. Santana</title>
+    <link rel='shortcut icon' href='imagens/favicon.ico' />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Lazarone Santana</title>
+    <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link href="stylesheets/stylesheets.css" rel="stylesheet">
+    <script src="javascripts/jquery-1.8.2.min.js" type="text/javascript"></script>
+    <script src="javascripts/javascripts.js" type="text/javascript"></script>
   </head>
   <body>
+<!--
     <div style='width: 100%; text-align: center;'>
       <h1>Site em  construção.</h1>
       E-mail: <a href='mailto:lazarone.info.web@gmail.com'>lazarone.info.web@gmail.com</a><br />
     </div>
+-->
+    <div id="topo">
+      <div id="logo-topo-principal">
+        <img src="imagens/logo_70.png" />
+      </div>
+      <div id="menu-topo-principal">
+  	<ul>
+  	  <li class="itens" id="item-1">
+  	    <a href="#secao-1">
+          Home
+          <div class="seta-identificacao"></div>
+        </a>
+  	  </li>
+  	  <li class="itens" id="item-2">
+  	    <a href="#secao-2">
+          Downloads
+          <div class="seta-identificacao"></div>
+        </a>
+  	  </li>
+  	  <li class="itens" id="item-3">
+  	    <a href="#secao-3">
+          Projetos
+          <div class="seta-identificacao"></div>
+        </a>
+  	  </li>
+  	  <li class="itens" id="item-4">
+  	    <a href="#secao-4">
+          Seção 4
+          <div class="seta-identificacao"></div>
+        </a>
+  	  </li>
+  	  <li class="itens" id="item-5">
+  	    <a href="#secao-5">
+          Seção 5
+          <div class="seta-identificacao"></div>
+        </a>
+  	  </li>
+  	</ul>
+      </div>
+    </div>
+    <div id="margem-topo">
+    </div>
+    <div id="secao-1" class="secao">
+    </div>
+    <div id="secao-2" class="secao">
+    <div>
+      <h1>Downloads</h1>
+    </div>
+    <div id="lado-secao-esq">
+      Este é um projeto do Charon, onde coloco o link para o youtube. Youtube.com.br/MeuCanal
+    </div>
+    <div id="lado-secao-dir">
+      <img src="imagens/rhea.png" />
+    </div>
+  </div>
+  <div id="secao-3" class="secao">
+    <div>
+      <h1>Projetos</h1>
+    </div>
+    <div id="lado-secao-esq">
+      <img src="imagens/rhea.png" />
+    </div>
+    <div id="lado-secao-dir">
+      Este é um projeto do Charon, onde coloco o link para o youtube. Youtube.com.br/MeuCanal
+    </div>
+  </div>
+  <div id="secao-4" class="secao">
+</div>
+<div id="secao-5" class="secao">
+</div>
+<div id="rodape"> 
+</div>
   <body>
   </body>
 </html>
